28162eded48359f7b88fefbbcfd49e70776b09c4
Manuel Pautasso <manuelpautasso@gmail.com>
2023-01-16 08:23:38 -0300

    First commit

---
+++ src/main/java/com/mpautasso/sistemavtv/SistemaVtvApplication.java
@@ -0,0 +1,13 @@
+package com.mpautasso.sistemavtv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaVtvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaVtvApplication.class, args);
+    }
+
+}
