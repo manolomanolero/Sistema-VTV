52a1a43cdd605a6177696db3ade2062ecbc34a82
Manuel Pautasso <manuelpautasso@gmail.com>
2023-02-06 15:52:45 -0300

    Added query on Modelo to get list by Marca, and query on Version to get list by Modelo

--- src/main/java/com/mpautasso/sistemavtv/repository/VersionRepository.java
+++ src/main/java/com/mpautasso/sistemavtv/repository/VersionRepository.java
@@ -1,9 +1,13 @@
 package com.mpautasso.sistemavtv.repository;
 
+import com.mpautasso.sistemavtv.model.Modelo;
 import com.mpautasso.sistemavtv.model.Version;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VersionRepository extends JpaRepository<Version, Long> {
+    List<Version> findAllByModelo(Modelo modelo);
 }
