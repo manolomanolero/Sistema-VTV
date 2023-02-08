0397ebc6354bfc70385ea9cc88dd6e73330f2b52
Manuel Pautasso <manuelpautasso@gmail.com>
2023-02-08 16:05:26 -0300

    Updated cross-origin to resolve cors problem

--- src/main/java/com/mpautasso/sistemavtv/controller/ClienteController.java
+++ src/main/java/com/mpautasso/sistemavtv/controller/ClienteController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/clientes")
+@CrossOrigin("*")
 public class ClienteController {
     private final ClienteService clienteService;
 
--- src/main/java/com/mpautasso/sistemavtv/controller/EmpleadoController.java
+++ src/main/java/com/mpautasso/sistemavtv/controller/EmpleadoController.java
@@ -15,6 +15,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/empleados")
+@CrossOrigin("*")
 public class EmpleadoController {
     private final EmpleadoService empleadoService;
 
